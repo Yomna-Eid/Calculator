620940673398db86e947ac67256e36989190fec5
yomna.eid <s-yomna.eid@zewailcity.edu.eg>
2018-12-04 13:13:17 +0100

    Addition

---
+++ R/addValue.R
@@ -0,0 +1,17 @@
+#' @title Addition
+#' @description A function that sums two values.
+#' 
+#' @param x First value
+#' @param y Second Value
+#' 
+#' @details Just play around.
+#' 
+#' @return Numeric value
+#' 
+#' @examples val <- addValue(1, 3)
+#' @export
+#' 
+
+addValue <- function(x, y){
+  return(x+y)
+}
